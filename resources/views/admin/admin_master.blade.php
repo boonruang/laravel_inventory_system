be13c94e448de897eadae69fa0b195a618478271
boonruang <scubatoy@gmail.com>
2022-10-15 02:48:29 +0700

    107. Supplier Page Setup Part 4

--- resources/views/admin/admin_master.blade.php
+++ resources/views/admin/admin_master.blade.php
@@ -128,7 +128,8 @@
         </script>
 
         <!-- Datatable init js -->
-        <script src="{{asset('backend/assets/js/pages/datatables.init.js')}}"></script>
+        <script src="{{ asset('backend/assets/js/pages/datatables.init.js') }}"></script>
+        <script src="{{ asset('backend/assets/js/validate.min.js') }}"></script>
 
     </body>
 
