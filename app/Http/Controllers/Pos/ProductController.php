b5122401cd69fb6feb68e6ef35d222dd32e03861
boonruang <scubatoy@gmail.com>
2022-10-16 13:47:18 +0700

    124. Products Page Setup Part 2

--- app/Http/Controllers/Pos/ProductController.php
+++ app/Http/Controllers/Pos/ProductController.php
@@ -5,8 +5,8 @@ namespace App\Http\Controllers\Pos;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Product;
-use App\Models\Category;
 use App\Models\Supplier;
+use App\Models\Category;
 use App\Models\Unit;
 use Auth;
 use Illuminate\Support\Carbon;
@@ -16,5 +16,12 @@ class ProductController extends Controller
     public function ProductAll(){
         $products = Product::latest()->get();
         return view('backend.product.product_all',compact('products'));
-    }
+    } // End Method
+
+    public function ProductAdd(){
+        $suppliers = Supplier::all();
+        $units = Unit::all();
+        $categories = Category::latest()->get();
+        return view('backend.product.product_add',compact('suppliers','units','categories'));
+    } // End Method
 }
