29efb34a67e7722f6851598f7ee4cc895dac481a
boonruang <scubatoy@gmail.com>
2022-10-16 22:01:07 +0700

    130. Purchase Management Part 2

--- app/Http/Controllers/Pos/PurchaseController.php
+++ app/Http/Controllers/Pos/PurchaseController.php
@@ -4,8 +4,20 @@ namespace App\Http\Controllers\Pos;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Purchase;
+use App\Models\Supplier;
+use App\Models\Product;
+use App\Models\Unit;
+use App\Models\Category;
+use Auth;
+use Illuminate\Support\Carbon;
 
 class PurchaseController extends Controller
 {
-    //
+    public function PurchaseAll(){
+
+        $allData = Purchase::orderBy('date','desc')->orderBy('id','desc')->get();
+
+        return view('backend.purchase.purchase_all',compact('allData'));
+    } //End Method
 }
