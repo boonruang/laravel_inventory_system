0df7c9800c3d3190c7eda275659caf02a3f291aa
boonruang <scubatoy@gmail.com>
2022-10-16 15:27:18 +0700

    126. Products Page Setup Part 4

--- app/Http/Controllers/Pos/ProductController.php
+++ app/Http/Controllers/Pos/ProductController.php
@@ -21,12 +21,11 @@ class ProductController extends Controller
     public function ProductAdd(){
         $suppliers = Supplier::all();
         $units = Unit::all();
-        $categories = Category::latest()->get();
+        $categories = Category::all();
         return view('backend.product.product_add',compact('suppliers','units','categories'));
     } // End Method
 
     public function ProductStore(Request $request) {
-        
         Product::insert([
             'name' => $request->name,
             'supplier_id' => $request->supplier_id,
@@ -41,8 +40,34 @@ class ProductController extends Controller
             'message' => 'Product Inserted Successfully',
             'alert-type' => 'success'
         );
-
         return redirect()->route('product.all')->with($notification);
+    } // End Method
+
+    public function ProductEdit($id) {
+        $product = Product::findOrFail($id);
+        $suppliers = Supplier::all();
+        $units = Unit::all();
+        $categories = Category::all();
+        return view('backend.product.product_edit',compact('product','suppliers','units','categories'));
+    } // End Method
+
+    public function ProductUpdate(Request $request){
+        $product_id = $request->id;
+
+        Product::findOrFail($product_id)->update([
+            'name' => $request->name,
+            'supplier_id' => $request->supplier_id,
+            'unit_id' => $request->unit_id,
+            'category_id' => $request->category_id,
+            'updated_by' => Auth::user()->id,
+            'updated_at' => Carbon::now(),            
+        ]);
+
+        $notification = array(
+            'message' => 'Product Updated Successfully',
+            'alert-type' => 'success'
+        );
+        return redirect()->route('product.all')->with($notification);        
 
     } // End Method
 }
