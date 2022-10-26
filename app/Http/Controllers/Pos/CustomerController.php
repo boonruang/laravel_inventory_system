0e7ff2ef609e5e9cf5936772a43de5ea1e5aab5d
boonruang <scubatoy@gmail.com>
2022-10-27 02:55:42 +0700

    184. Customer Edit Invoice Part 1

--- app/Http/Controllers/Pos/CustomerController.php
+++ app/Http/Controllers/Pos/CustomerController.php
@@ -128,5 +128,10 @@ class CustomerController extends Controller
     public function CreditCustomerPrintPdf(){
         $allData = Payment::whereIn('paid_status',['full_due','partial_paid'])->get();
         return view('backend.pdf.customer_credit_pdf',compact('allData'));
-    }
+    } // End Method
+
+    public function CustomerEditInvoice($invoice_id) {
+        $payment = Payment::where('invoice_id',$invoice_id)->first();
+        return view('backend.customer.edit_customer_invoice',compact('payment'));
+    } // End Method
 }
